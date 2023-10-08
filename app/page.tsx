9c428c8f32f12d12c0a8f546cdce39f942cb954b
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-10-08 14:15:43 +0800

    Main section two complete

--- app/page.tsx
+++ app/page.tsx
@@ -34,10 +34,11 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="mt-[6rem] w-[90%] mx-auto sm:w-[100%]">
-      <section className="main-one">
+    <main className="mt-[6rem]">
+      {/* SECTION ONE */}
+      <section className="main-one sm:px-0 px-[2rem]">
         <div className='title-container'>
-          <h1 className=' text-[2rem] font-ubuntu-bold text-center'>Designed for the future</h1>
+          <h1 className=' text-[2rem] font-ubuntu-bold text-center text-very-dark-blue'>Designed for the future</h1>
         </div>
         <div className='body-container flex flex-col sm:flex-row-reverse sm:pl-[5rem] lg:pl-[10rem] pt-[3rem] justify-center items-center'>
           <div className='image-container sm:flex-1 overflow-hidden'>
@@ -46,20 +47,20 @@ export default function Home() {
               alt="illustration editor"
               width={imageWidth}
               height={imageHeight}
-              className='illustrator sm:max-w-[140%]'
+              className='illustrator sm:max-w-[140%] lg:max-w-[160%]'
             />
           </div>
 
-          <div className='text-container sm:flex-1 pt-[3rem] text-center sm:text-left'>
-            <h2 className='text-[1.5rem] font-ubuntu-bold'>Introducing an extensible editor</h2>
-            <p className='text-[1.1rem] pt-[.8rem]'>
+          <div className='text-container sm:flex-1 pt-[3.5rem] text-center sm:text-left sm:pr-[3rem] lg:pr-[10rem]'>
+            <h2 className='text-[1.5rem] font-ubuntu-bold text-very-dark-blue'>Introducing an extensible editor</h2>
+            <p className='text-[1.1rem] pt-[.8rem] text-very-dark-grayish-blue leading-[2rem]'>
               Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
               The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
               videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
               change the looks of a blog.
             </p>
-            <h2 className='text-[1.5rem] font-ubuntu-bold pt-[1rem]'>Robust content management</h2>
-            <p className='text-[1.1rem] pt-[.8rem]'>
+            <h2 className='text-[1.5rem] font-ubuntu-bold pt-[3.5rem] text-very-dark-blue'>Robust content management</h2>
+            <p className='text-[1.1rem] pt-[.8rem] text-very-dark-grayish-blue leading-[2rem]'>
               Flexible content management enables users to easily move through posts. Increase the usability of your blog
               by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
             </p>
@@ -67,12 +68,31 @@ export default function Home() {
         </div>
       </section>
 
+      {/* SECTION TWO */}
+      <section className='main-two mt-[17rem] sm:mt-[6rem] flex flex-col sm:flex-row items-center'>
+        <div className='image-container flex justify-center flex-1 -mt-[12rem] sm:mt-0'>
+          <Image 
+            src='/illustration-phones.svg'
+            alt='phone'
+            height={579}
+            width={552}
+            className='img-phone'
+          />
+        </div>
+
+        <div className="text-container flex-1 px-[2rem] text-center sm:text-left xl:pr-[7rem] -mt-[3rem] sm:mt-0 pb-[7rem] sm:pb-0">
+          <h2 className='text-[2rem] font-ubuntu-bold pt-[3.5rem] text-white'>State of the Art Infrastructure</h2>
+          <p className='text-[1rem] pt-[.8rem] text-white font-light leading-[2rem]'>
+            With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity.
+            This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
+          </p>
+        </div>
+      </section>
+
+      {/* SECTION THREE NAMAN NEXT */}
+
       {/* <div className="main-two">
-        <h1>State of the Art Infrastructure</h1>
-        <p>
-          With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity.
-          This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
-        </p>
+        
       </div>
 
       <div className="main-three">
